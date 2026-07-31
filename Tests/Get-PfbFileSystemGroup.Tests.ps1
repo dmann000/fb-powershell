39ddb25888367420c48f6b7f9f01639238b5284f
Justin Emerson <Justin.Emerson@everpuredata.com>
2026-07-31 00:12:09 -0700

    feat: add Get-PfbFileSystemGroup

---
+++ Tests/Get-PfbFileSystemGroup.Tests.ps1
@@ -0,0 +1,50 @@
+#Requires -Modules @{ ModuleName = 'Pester'; ModuleVersion = '5.0' }
+
+BeforeAll {
+    $moduleRoot = Split-Path -Parent $PSScriptRoot
+    $manifest   = Join-Path $moduleRoot 'PureStorageFlashBladePowerShell.psd1'
+    Import-Module $manifest -Force
+
+    $script:fakeArray = [PSCustomObject]@{ Endpoint = 'fb.example.test'; ApiVersion = '2.25'; AuthToken = 'x' }
+}
+
+Describe 'Get-PfbFileSystemGroup' {
+
+    BeforeEach {
+        Mock -ModuleName PureStorageFlashBladePowerShell Assert-PfbConnection { }
+        Mock -ModuleName PureStorageFlashBladePowerShell Invoke-PfbApiRequest { }
+    }
+
+    It 'GETs file-systems/groups with AutoPaginate' {
+        InModuleScope PureStorageFlashBladePowerShell -Parameters @{ arr = $fakeArray } {
+            param($arr)
+            Get-PfbFileSystemGroup -Array $arr
+        }
+
+        Should -Invoke -ModuleName PureStorageFlashBladePowerShell Invoke-PfbApiRequest -Times 1 -Exactly -ParameterFilter {
+            $Method -eq 'GET' -and $Endpoint -eq 'file-systems/groups' -and $AutoPaginate -eq $true
+        }
+    }
+
+    It 'filters by -FileSystemName' {
+        InModuleScope PureStorageFlashBladePowerShell -Parameters @{ arr = $fakeArray } {
+            param($arr)
+            Get-PfbFileSystemGroup -FileSystemName 'fs1' -Array $arr
+        }
+
+        Should -Invoke -ModuleName PureStorageFlashBladePowerShell Invoke-PfbApiRequest -Times 1 -Exactly -ParameterFilter {
+            $QueryParams['file_system_names'] -eq 'fs1'
+        }
+    }
+
+    It 'sends -GroupName as group_names and -GroupId as gids' {
+        InModuleScope PureStorageFlashBladePowerShell -Parameters @{ arr = $fakeArray } {
+            param($arr)
+            Get-PfbFileSystemGroup -GroupName 'staff' -GroupId '2001' -Array $arr
+        }
+
+        Should -Invoke -ModuleName PureStorageFlashBladePowerShell Invoke-PfbApiRequest -Times 1 -Exactly -ParameterFilter {
+            $QueryParams['group_names'] -eq 'staff' -and $QueryParams['gids'] -eq '2001'
+        }
+    }
+}
