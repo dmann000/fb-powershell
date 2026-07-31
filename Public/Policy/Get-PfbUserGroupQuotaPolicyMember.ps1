e65256fd6d6994d557db0e0c3cb81091c779f605
Justin Emerson <Justin.Emerson@everpuredata.com>
2026-07-30 23:29:33 -0700

    feat: add Get-PfbUserGroupQuotaPolicyMember

---
+++ Public/Policy/Get-PfbUserGroupQuotaPolicyMember.ps1
@@ -0,0 +1,53 @@
+function Get-PfbUserGroupQuotaPolicyMember {
+    <#
+    .SYNOPSIS
+        Retrieves user/group quota policy member associations from a FlashBlade array.
+    .DESCRIPTION
+        The Get-PfbUserGroupQuotaPolicyMember cmdlet returns the cross-reference between
+        user-group-quota policies and their members (file systems) on the connected Pure
+        Storage FlashBlade. This is a read-only view; use Get-PfbUserGroupQuotaPolicyFileSystem
+        to see the same relationship from the file-system-attachment endpoint.
+    .PARAMETER PolicyName
+        One or more policy names to filter by.
+    .PARAMETER PolicyId
+        One or more policy IDs to filter by.
+    .PARAMETER MemberName
+        One or more member names to filter by.
+    .PARAMETER MemberId
+        One or more member IDs to filter by.
+    .PARAMETER Filter
+        A server-side filter expression to narrow results.
+    .PARAMETER Sort
+        Sort field and direction.
+    .PARAMETER Limit
+        Maximum number of entries to return.
+    .PARAMETER Array
+        The FlashBlade connection object. If not specified, the default connection is used.
+    .EXAMPLE
+        Get-PfbUserGroupQuotaPolicyMember -PolicyName "quota-pol-1"
+    #>
+    [CmdletBinding()]
+    param(
+        [Parameter()] [string[]]$PolicyName,
+        [Parameter()] [string[]]$PolicyId,
+        [Parameter()] [string[]]$MemberName,
+        [Parameter()] [string[]]$MemberId,
+        [Parameter()] [string]$Filter,
+        [Parameter()] [string]$Sort,
+        [Parameter()] [int]$Limit,
+        [Parameter()] [PSCustomObject]$Array
+    )
+
+    Assert-PfbConnection -Array ([ref]$Array)
+
+    $queryParams = @{}
+    if ($PolicyName) { $queryParams['policy_names'] = $PolicyName -join ',' }
+    if ($PolicyId)   { $queryParams['policy_ids']   = $PolicyId -join ',' }
+    if ($MemberName) { $queryParams['member_names'] = $MemberName -join ',' }
+    if ($MemberId)   { $queryParams['member_ids']   = $MemberId -join ',' }
+    if ($Filter) { $queryParams['filter'] = $Filter }
+    if ($Sort)   { $queryParams['sort']   = $Sort }
+    if ($Limit -gt 0) { $queryParams['limit'] = $Limit }
+
+    Invoke-PfbApiRequest -Array $Array -Method GET -Endpoint 'user-group-quota-policies/members' -QueryParams $queryParams -AutoPaginate
+}
