741d01445c269627f6b4ce8bbd22d438ee7fa45b
Justin Emerson <Justin.Emerson@everpuredata.com>
2026-07-31 00:07:06 -0700

    feat: add Get-PfbFileSystemUser

---
+++ Tests/Get-PfbFileSystemUser.Tests.ps1
@@ -0,0 +1,50 @@
+#Requires -Modules @{ ModuleName = 'Pester'; ModuleVersion = '5.0' }
+
+BeforeAll {
+    $moduleRoot = Split-Path -Parent $PSScriptRoot
+    $manifest   = Join-Path $moduleRoot 'PureStorageFlashBladePowerShell.psd1'
+    Import-Module $manifest -Force
+
+    $script:fakeArray = [PSCustomObject]@{ Endpoint = 'fb.example.test'; ApiVersion = '2.25'; AuthToken = 'x' }
+}
+
+Describe 'Get-PfbFileSystemUser' {
+
+    BeforeEach {
+        Mock -ModuleName PureStorageFlashBladePowerShell Assert-PfbConnection { }
+        Mock -ModuleName PureStorageFlashBladePowerShell Invoke-PfbApiRequest { }
+    }
+
+    It 'GETs file-systems/users with AutoPaginate' {
+        InModuleScope PureStorageFlashBladePowerShell -Parameters @{ arr = $fakeArray } {
+            param($arr)
+            Get-PfbFileSystemUser -Array $arr
+        }
+
+        Should -Invoke -ModuleName PureStorageFlashBladePowerShell Invoke-PfbApiRequest -Times 1 -Exactly -ParameterFilter {
+            $Method -eq 'GET' -and $Endpoint -eq 'file-systems/users' -and $AutoPaginate -eq $true
+        }
+    }
+
+    It 'filters by -FileSystemName' {
+        InModuleScope PureStorageFlashBladePowerShell -Parameters @{ arr = $fakeArray } {
+            param($arr)
+            Get-PfbFileSystemUser -FileSystemName 'fs1' -Array $arr
+        }
+
+        Should -Invoke -ModuleName PureStorageFlashBladePowerShell Invoke-PfbApiRequest -Times 1 -Exactly -ParameterFilter {
+            $QueryParams['file_system_names'] -eq 'fs1'
+        }
+    }
+
+    It 'sends -UserName as user_names, -UserId as uids, and -UserSid as user_sids' {
+        InModuleScope PureStorageFlashBladePowerShell -Parameters @{ arr = $fakeArray } {
+            param($arr)
+            Get-PfbFileSystemUser -UserName 'jdoe' -UserId '1001' -UserSid 'S-1-5-1' -Array $arr
+        }
+
+        Should -Invoke -ModuleName PureStorageFlashBladePowerShell Invoke-PfbApiRequest -Times 1 -Exactly -ParameterFilter {
+            $QueryParams['user_names'] -eq 'jdoe' -and $QueryParams['uids'] -eq '1001' -and $QueryParams['user_sids'] -eq 'S-1-5-1'
+        }
+    }
+}
